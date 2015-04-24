33a74e962704393d7abb5393f2519533f2f851ab
Lilooo <alice.giordani@gmail.com>
2015-04-24 11:46:12 +0200

    Init v1
    Blue sine wave interference pattern

---
+++ Quiet_River.ino
@@ -0,0 +1,45 @@
+/* Quiet-River
+By: Alice
+inspired by : Andrew Tuline
+Date: Avril 2015
+Blue sine wave interference pattern.
+*/
+
+#include "FastLED.h"
+
+// Fixed definitions
+#define DATA_PIN 6
+#define CLOCK_PIN 11
+#define COLOR_ORDER RGB                                      
+#define LED_TYPE NEOPIXEL
+#define NUM_LEDS 29
+
+CRGB leds[NUM_LEDS];
+
+// The following variables can be changed.
+int wave1=0;
+int wave2=0;
+int wave3=0;
+
+void setup() {
+  // Use this for WS2801 Leds
+  //  LEDS.addLeds<LED_TYPE, DATA_PIN, CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+
+  // Use this for NEOPIXEL
+  LEDS.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);  
+}
+
+
+void loop() {
+  // Waves of different frequencies
+  wave1+=835;                                    
+  wave2-=693;                                    
+  wave3+=493;
+
+  for (int k=0; k<NUM_LEDS; k++) {
+    //Three combined waves (added)
+   leds[k].b = ((sin16(wave1+k*wave1)/256+128) + (sin16(wave2+k*wave2)/256+128)+ (sin16(wave3+k*wave3)/256+128))/4;    
+  }
+  LEDS.show();
+  delay(60);
+}
